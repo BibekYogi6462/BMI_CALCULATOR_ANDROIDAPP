c8826e445aa60fd862fbf64ce697077e1570fb89
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-09-15 13:17:28 +0545

    Initial commit: BMI Calculator Android App

---
+++ app/src/main/java/com/by/bmicaluculator/MainActivity.java
@@ -0,0 +1,74 @@
+package com.by.bmicaluculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.llMain), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText edtWeight, edtHeightFt, edtHeightIn;
+        Button btnCalculate;
+        TextView txtResult;
+        LinearLayout llMain;
+
+        edtWeight = findViewById(R.id.edtWeight);
+        edtHeightFt = findViewById(R.id.edtHeightFt);
+        edtHeightIn = findViewById(R.id.edtHeightIn);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtResult = findViewById(R.id.txtResult);
+        llMain = findViewById(R.id.llMain);
+
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int wt = Integer.parseInt( edtWeight.getText().toString());
+                int ft = Integer.parseInt(edtHeightFt.getText().toString());
+                int in = Integer.parseInt(edtHeightIn.getText().toString());
+
+                int totalIn = ft*12 +in;
+                double totalCm = totalIn*2.53;
+                double totalM = totalCm/100;
+
+                double bmi = wt/(totalM*totalM);
+
+
+                if (bmi > 25) {
+                    txtResult.setText("You are Overweight");
+                    llMain.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorO));
+                } else if (bmi < 18) {
+                    txtResult.setText("You are Underweight");
+                    llMain.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.coloeUW));
+                } else {
+                    txtResult.setText("You are Healthy");
+                    llMain.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorH));
+                }
+
+            }
+        });
+    }
+}
